2693bdef9deb46cab3c317944fca82b6533f4e72
Robert Bunch <rob@digitalcrafts.com>
2016-04-25 12:37:19 -0400

    Add a ton of comments and add a post route

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 
-Steps
-1. Initialize the app with the express generator.
--- express electric-or-not
--- This will create an entire folder structure with everything we need to get started making an epxress web (dub dub dub) application
-2. add a .gitignore file that will (at least) ignore everythign inside of node_modules
--- reason for this? If someone wants to run your app, they can do an npm install themselves. These moduels should not be committed to the repository unless there is a very good reason. 
-3. Copy the compass boilerplate into the project.
--- this will give you access to compass. We need to change teh rb config file to write to teh correct place.
--- -- This means changing the path of the css write directory to ../public/stylesheets
-4. Init git the repo
--- from teh command line:
--- -- git init
--- -- git add * (this will add ALL files in teh directory to be commited)
--- -- git commit -m "Init commit to github"
--- -- git push/add origin/open up github desktop
-5. npm install ejs --save
-6. npm install mongodb --save
--- These two steps will get two more modules from the npm market for us to use in our app.
-7. npm install
--- this will install express, all it's dependecies, etc. Whatever is inisde of pacakge.json
-8. SANITY CHECK. RUN NODEMON.
+// Steps
+// 1. Initialize the app with the express generator.
+// -- express electric-or-not
+// -- This will create an entire folder structure with everything we need to get started making an epxress web (dub dub dub) application
+// 2. add a .gitignore file that will (at least) ignore everythign inside of node_modules
+// -- reason for this? If someone wants to run your app, they can do an npm install themselves. These moduels should not be committed to the repository unless there is a very good reason. 
+// 3. Copy the compass boilerplate into the project.
+// -- this will give you access to compass. We need to change teh rb config file to write to teh correct place.
+// -- -- This means changing the path of the css write directory to ../public/stylesheets
+// 4. Init git the repo
+// -- from the command line:
+// -- -- git init
+// -- -- git add * (this will add ALL files in teh directory to be commited)
+// -- -- git commit -m "Init commit to github"
+// -- -- git add remote origin/open up github desktop
+// 5. npm install ejs --save
+// 6. npm install mongodb --save
+// -- These two steps will get two more modules from the npm market for us to use in our app.
+// 7. npm install
+// -- this will install express, all it's dependecies, etc. Whatever is inisde of pacakge.json
+// 8. SANITY CHECK. RUN NODEMON.
+// 9. Switch the templating engine (if desired) from Jade.
+// -- in app.js (where we are at) go down to the app.set and change it from jade, to ejs.
+// -- in the views folder, change .jade files to .ejs files.
+// 10. In index.ejs, set up our common files and include themselves
+// - head
+// - nav
+// - footer
+// 11. Set up a wrapper div to hold our voting buttons and our image
+// 12. Style the home page
+// 13. Set up an test the connection to Mongo.
 
 
 var express = require('express');
@@ -36,7 +46,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Set up the post electric page. */
+router.post('/electric', function(req, res, next){
+	res.send(req.body);
+});
+
 module.exports = router;
