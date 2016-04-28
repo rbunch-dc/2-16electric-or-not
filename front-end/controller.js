c3848292e4ac836b5fb381c1876078311e84f8a6
Ubuntu <ubuntu@ip-172-31-11-131.ec2.internal>
2016-04-28 18:24:47 +0000

    add front end directory

---
+++ front-end/controller.js
@@ -0,0 +1,37 @@
+var searchApp = angular.module('searchApp', []);
+
+searchApp.controller('searchController', function($scope, $http){
+	var apiUrl = 'http://localhost:3000/search';		
+	$http({
+           method: 'GET',
+           url: apiUrl
+       }).then(
+		function successCallback(response) {
+			$scope.charlie = response.data;
+			console.log($scope.studentsOnLoad);
+ 			}, function errorCallback(response) {
+ 				$scope.result = "ERROR!!!"
+ 			});
+
+
+
+	$scope.search = function(){
+		var apiUrl = 'http://10.150.41.253:3000/search';		
+		$http({
+            method: 'POST',
+            url: apiUrl,
+            data: {name: $scope.who}
+        }).then(
+			function successCallback(response) {
+				$scope.result = response.data;
+    			// this callback will be called asynchronously
+    			// when the response is available
+  			}, function errorCallback(response) {
+  				$scope.result = "ERROR!!!"
+    			// called asynchronously if an error occurs
+    			// or server returns response with an error status.
+  			});
+	}
+
+});
+
